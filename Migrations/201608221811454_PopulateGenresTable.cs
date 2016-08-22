ce8dde04e3aeff64d3d9b33fe1fe46792f2f49bc
rmoncrief <rmoncri1@vols.utk.edu>
2016-08-22 14:19:25 -0400

    Added first View

---
+++ Migrations/201608221811454_PopulateGenresTable.cs
@@ -0,0 +1,21 @@
+namespace Gighub.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateGenresTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Genres (Id, Name) Values (1, 'Jazz')");
+            Sql("INSERT INTO Genres (Id, Name) Values (2, 'Blues')");
+            Sql("INSERT INTO Genres (Id, Name) Values (3, 'Rock')");
+            Sql("INSERT INTO Genres (Id, Name) Values (4, 'Country')");
+        }
+        
+        public override void Down()
+        {
+            Sql("DELETE FROM Genres Where Id IN (1,2,3,4)");
+        }
+    }
+}
